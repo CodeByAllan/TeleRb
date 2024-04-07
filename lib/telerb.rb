49a5ad7c73c36dc4e5d7ef9bfc24e49e8353ea9d
CodeByAllan <allan.pereira.dev@proton.me>
2024-04-07 10:04:43 -0300

    add error handling to methods

--- lib/telerb.rb
+++ lib/telerb.rb
@@ -24,13 +24,19 @@ module TeleRb
           block.call update["message"] if block_given? && update["message"]
           @offset = update["update_id"] + 1
         end
-        sleep 2
+      rescue StandardError => e
+        puts "Error in listen method: #{e.message}"
       end
+      sleep 2
     end
 
     def send_message(chat_id, text, reply_to_message_id = nil)
       CLIENT.post("#{@base_uri}#{@token}/sendMessage",
                   { chat_id: chat_id, text: text, reply_to_message_id: reply_to_message_id })
+      "Message sent successfully!"
+    rescue StandardError => e
+      puts "Error sending message: #{e.message}"
+      nil
     end
 
     def send_photo(chat_id, photo_path, caption = nil, reply_to_message_id = nil)
@@ -42,11 +48,18 @@ module TeleRb
                     caption: caption
                   },
                   { "Content-Type" => "multipart/form-data" })
+      "Photo sent successfully!"
+    rescue StandardError => e
+      puts "Error sending photo: #{e.message}"
+      nil
     end
 
     def get_updates(offset = nil)
       response = CLIENT.get("#{@base_uri}#{@token}/getUpdates", { offset: offset })
       response.parsed_response["result"]
+    rescue StandardError => e
+      puts "Error in get_updates method: #{e.message}"
+      []
     end
   end
 end
