f789e04251b792840617cd03f17b6e622fdedda9
CodeByAllan <allan.pereira.dev@proton.me>
2024-04-10 17:31:48 -0300

    Implement the method to obtain information from a user

--- spec/telerb_spec.rb
+++ spec/telerb_spec.rb
@@ -89,4 +89,53 @@ RSpec.describe TeleRb::TelegramBot do
       expect(bot.forward_message(from_chat_id, to_chat_id, message_id)).to eq("Forwarding was then done successfully!")
     end
   end
+  describe "#user_info" do
+    let(:message) do
+      {
+        "message_id" => 1,
+        "from" => {
+          "id" => 1234,
+          "is_bot" => false,
+          "first_name" => "exemple",
+          "last_name" => "test",
+          "username" => "exemple",
+          "language_code" => "en-us"
+        },
+        "chat" => {
+          "id" => 1234,
+          "first_name" => "exemple",
+          "last_name" => "test",
+          "username" => "exemple",
+          "type" => "private"
+        },
+        "date" => "123456789",
+        "text" => "hello"
+      }
+    end
+
+    let(:expected_user_info) do
+      {
+        "id" => 1234,
+        "is_bot" => false,
+        "first_name" => "exemple",
+        "last_name" => "test",
+        "username" => "exemple",
+        "language_code" => "en-us"
+      }
+    end
+
+    it "returns correct user info" do
+      expect(bot.user_info(message)).to eq(expected_user_info)
+    end
+
+    it "returns a Hash" do
+      expect(bot.user_info(message)).to be_a(Hash)
+    end
+
+    it "does not modify the original message" do
+      original = message.dup
+      bot.user_info(message)
+      expect(message).to eq(original)
+    end
+  end
 end
