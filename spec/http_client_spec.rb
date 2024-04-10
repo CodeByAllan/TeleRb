fbe8ff15d55602ff313c2296ab1c151e81b8c825
CodeByAllan <allan.pereira.dev@proton.me>
2024-04-10 19:04:23 -0300

    better organization of tests

--- spec/http_client_spec.rb
+++ spec/http_client_spec.rb
@@ -4,19 +4,15 @@ require "./lib/telerb/http_client"
 
 RSpec.describe HttpClient do
   let(:client) { HttpClient.new }
-  describe "#get" do
-    it "http get " do
-      url = "exemple.com"
-      allow(client).to receive(:get).with(url).and_return("Hello World!")
-      expect(client.get(url)).to eq("Hello World!")
-    end
+  it "http get " do
+    url = "exemple.com"
+    allow(client).to receive(:get).with(url).and_return("Hello World!")
+    expect(client.get(url)).to eq("Hello World!")
   end
-  describe "#post" do
-    it "http post " do
-      url = "exemple.com"
-      body = { name: "ruby" }
-      allow(client).to receive(:post).with(url, body).and_return("Sucess!")
-      expect(client.post(url, body)).to eq("Sucess!")
-    end
+  it "http post " do
+    url = "exemple.com"
+    body = { name: "ruby" }
+    allow(client).to receive(:post).with(url, body).and_return("Sucess!")
+    expect(client.post(url, body)).to eq("Sucess!")
   end
 end
